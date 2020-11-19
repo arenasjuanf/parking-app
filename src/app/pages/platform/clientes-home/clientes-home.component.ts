45f62db7985b974f71fc8dd08fd377236ed52f99
Juanf <juanfa107@gmail.com>
2020-11-19 00:18:54 -0500

    cambios en cliente

--- src/app/pages/platform/clientes-home/clientes-home.component.ts
+++ src/app/pages/platform/clientes-home/clientes-home.component.ts
@@ -4,6 +4,7 @@ import { zip } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
 import { constantes } from 'src/app/constantes';
 import { AuthService } from '../services/auth.service';
+import { DatabaseService } from '../services/database.service';
 
 @Component({
   selector: 'app-clientes-home',
@@ -17,9 +18,10 @@ export class ClientesHomeComponent implements OnInit {
   parqueaderos: any = [];
   configLoader = constantes.coloresLoader;
   cargando: boolean = false;
+  mostrarParking: boolean = false;
 
 
-  constructor(private auth: AuthService, private afs: AngularFirestore) {
+  constructor(private auth: AuthService, private afs: AngularFirestore, private db: DatabaseService) {
     this.datosParqueadero = this.auth.datosUsuario;
     this.traerParqueaderos(this.datosParqueadero.parqueadero);
   }
@@ -38,36 +40,55 @@ export class ClientesHomeComponent implements OnInit {
 
     this.afs.collection(`/parqueaderos`, ref => 
       ref.where('clientesActivos', 'array-contains', this.auth.datosUsuario.key)
-    ).valueChanges().pipe(
-      map( x => x.map( p => {
+    ).snapshotChanges().pipe(
+      map( (x:any) => x.map( p => {
         return {
-          razonSocial: p['razonSocial'],
-          logo: p['logo'],
-          direccion: p['direccion']
+          key: p.payload.doc.id,
+          razonSocial: p.payload.doc.get('razonSocial'),
+          logo: p.payload.doc.get('logo'),
+          direccion: p.payload.doc.get('direccion')
         };
       }))
     ).subscribe((result: any) => {
       this.parqueaderos = result;
       this.cargando = false;
     });
+  }
 
 
-  /*   ids.forEach(id => {
-      observables.push( this.afs.collection('parqueaderos').doc(id).valueChanges() );
-    }); */
 
-   /*  zip(...observables).pipe(
-      map( elem => elem.map(x => {
-        return {
-          razonSocial: x['razonSocial'],
-          logo: x['logo'],
-          direccion: x['direccion']
-        };
-      }))
-    ).subscribe((result: any) => {
-      console.log(result);
-      this.parqueaderos = result;
-      this.cargando = false;
-    }); */
+  seleccionarParqueadero(parqueadero){
+    this.mostrarParking = true;
+    const idParqueadero = parqueadero.key;
+
+    this.db.findDoc('parqueaderos', idParqueadero).snapshotChanges().subscribe(result => {
+      if (result){
+        const plano = JSON.parse(result.payload.get('plano'));
+        this.getSubs(plano);
+      }
+    }, error => {
+      console.log('error: ', error);
+    });
+  }
+
+  getSubs(plano: any[]) {
+    const idUser = this.auth.datosUsuario.key;
+    const suscripciones: any[] = [];
+
+    plano.forEach(piso => {
+      piso.forEach(fila => {
+        fila.forEach(casilla => {
+          if(casilla.suscripcion){
+            const subs = casilla.suscripcion;
+            if (subs.vehiculo.usuario === this.auth.datosUsuario.key){
+              suscripciones.push(subs);
+            }
+          }
+        });
+      });
+    });
+
+    console.log(suscripciones)
+
   }
 }
